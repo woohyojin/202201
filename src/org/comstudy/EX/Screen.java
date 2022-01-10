6284b0e08cf85731335e3d03832080f1f1b165eb
woohyojin <96456803+woohyojin@users.noreply.github.com>
2022-01-11 02:04:05 +0900

    Update Screen.java

--- src/org/comstudy/EX/Screen.java
+++ src/org/comstudy/EX/Screen.java
@@ -1,6 +1,7 @@
 package org.comstudy.EX;
 
 import java.awt.BorderLayout;
+
 import java.awt.Button;
 import java.awt.FlowLayout;
 import java.awt.GridBagLayout;
@@ -10,6 +11,10 @@ import java.awt.Panel;
 import java.awt.TextField;
 import java.awt.font.TextLayout;
 
+import javax.swing.border.Border;
+
+import java.awt.TextArea;
+
 import org.comstudy.guiex.myframe.MyFrame;
 
 public class Screen extends MyFrame {
@@ -17,47 +22,41 @@ public class Screen extends MyFrame {
 	TextField tf2;
 	TextField tf3;
 	TextField tf4;
-	
+
 	Panel southPan = new Panel(new FlowLayout());
-	Panel westPan = new Panel(new GridLayout(4,2,5,5));
-	Panel centerPan = new Panel(new GridLayout(1,1,10,10));
+	Panel westPan = new Panel(new GridLayout(4, 2, 5, 5));
+	Panel centerPan = new Panel(new FlowLayout());
 	Label Lb1 = new Label("번호");
 	Label Lb2 = new Label("이름");
 	Label Lb3 = new Label("이메일");
 	Label Lb4 = new Label("전화번호");
-	
-	
+
 	Button btn1 = new Button("전체보기");
 	Button btn2 = new Button("추가");
 	Button btn3 = new Button("삭제");
 	Button btn4 = new Button("검색");
 	Button btn5 = new Button("취소");
-	
-	
-	Button btn10 = new Button("");
-	
-	
-	
-	
+
+	TextArea area = new TextArea("", 10, 10, 2);
+
 	public Screen() {
-		GridBagLayoutEx1();
-		GridBagLayoutEx2();
-		GridBagLayoutEx3();
-		
+		FlowLayoutEx();
+		GridLayoutEx();
+		CenterEx();
+
 	}
 
-	public void GridBagLayoutEx3() {
-		centerPan.add(btn10);
-		this.add(BorderLayout.CENTER, centerPan);
-		
+	public void CenterEx() {
+		add(BorderLayout.CENTER, centerPan);
+		centerPan.add(area);
 	}
 
-	public void GridBagLayoutEx2() {
+	public void GridLayoutEx() {
 		tf1 = new TextField(20);
 		tf2 = new TextField(20);
 		tf3 = new TextField(20);
 		tf4 = new TextField(20);
-		
+
 		westPan.add(Lb1);
 		westPan.add(tf1);
 		westPan.add(Lb2);
@@ -67,17 +66,17 @@ public class Screen extends MyFrame {
 		westPan.add(Lb4);
 		westPan.add(tf4);
 		this.add(BorderLayout.WEST, westPan);
-		
+
 	}
 
-	public void GridBagLayoutEx1() {
+	public void FlowLayoutEx() {
 		southPan.add(btn1);
 		southPan.add(btn2);
 		southPan.add(btn3);
 		southPan.add(btn4);
-		southPan.add(btn5);	
+		southPan.add(btn5);
 		this.add(BorderLayout.SOUTH, southPan);
-		
+
 	}
 
 	public static void main(String[] args) {
